3e9f929bba9c608ec96fcc7f757d30263a8872d7
33-66 <simon.nganga@student.moringaschool.com>
2023-12-11 12:13:40 +0300

    work done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
+
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page(self):
+      return  self._page_count
+
+    def set_page(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_page, set_page)
+
     pass
-    
-        
+
+
+book = Book("today", "272")
+print(book.title == "today")
+print(book.title)
+print(book.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    # Property definition
+    size = property(get_size, set_size)
+
+
+stan_smith = Shoe("Adidas", "s")  # Passing a string "s" as size
+print(
+    stan_smith.size
+)  # This will print 'None' because the size wasn't set to an integer
+stan_smith.size = "y"  # Setting size to an integer value
+print(
+    stan_smith.size
+)  # Now it will print the size value set previously (9 in this case)
